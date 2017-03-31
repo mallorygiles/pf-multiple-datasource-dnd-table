a04d660ca4e22689df312ea8dd4efd3c86d6c8c5
mallorygiles <mallory_giles@sra.com>
2017-03-31 09:42:33 -0400

    Add example attempt to drag and drop between multiple tables

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-![PrimeFaces icon](https://www.primefaces.org/wp-content/uploads/2016/10/prime_logo.png)
-
-
-This is a sample maven project that used <strong>PF6.0</strong> version. If you have a PrimeFaces issue, please download or fork this project. Then, you should change these files by yourself so that PrimeFaces Team can see your problem. Finally, you can send a link or attach the project. <strong>Please pay attention that it in runnable condition.</strong>
-
-You can execute the sample with <strong>mvn jetty:run</strong> command and hit <strong>http://localhost:8080/primefaces-test</strong> to run the page.
+This is a sample project where I would like to be able to drag and drop rows between multiple tables. The issue is that
+if I use multiple <p:droppable> for the same component, only the last one is used. Therefore, if you try to drag from
+table whose datasource was given in the first <p:droppable>, it will actually use the datasource of the last one. For
+example, for table 2, the droppables are the datasource for table 1 then for table 3. If I attempt to drag from table 1 
+to table 2, it will actually take the row from table 3. If there are only 3 columns in table 3 and I attempt to drag the 
+4th column from table 1, then I receive a javax.faces.model.NoRowAvailableException.
--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,26 +1,84 @@
 package org.primefaces.test;
 
-import java.io.Serializable;
+import org.primefaces.event.DragDropEvent;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ManagedBean(name = "testView")
 @ViewScoped
 public class TestView implements Serializable {
-    
-    private String testString;
-    
-    @PostConstruct  
+
+    private List<String> availableStrings = new ArrayList<>();
+
+    @PostConstruct
     public void init() {
-        testString = "Welcome to PrimeFaces!!!";
+        availableStrings.add("A-tbl1");
+        availableStrings.add("B-tbl1");
+        availableStrings.add("C-tbl1");
+        availableStrings.add("D-tbl1");
+        availableStrings.add("E-tbl2");
+        availableStrings.add("F-tbl2");
+        availableStrings.add("G-tbl2");
+        availableStrings.add("H-tbl3");
+        availableStrings.add("I-tbl3");
+        availableStrings.add("J-tbl3");
+        availableStrings.add("K-tbl3");
+        availableStrings.add("L-tbl3");
+        availableStrings.add("M-tbl3");
+
+
     }
 
-    public String getTestString() {
-        return testString;
+    public List<String> getTbl1Strings(){
+        return availableStrings.stream()
+                .filter(s -> s.endsWith("tbl1"))
+                .collect(Collectors.toList());
     }
 
-    public void setTestString(String testString) {
-        this.testString = testString;
-    }    
+    public List<String> getTbl2Strings(){
+        return availableStrings.stream()
+                .filter(s -> s.endsWith("tbl2"))
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getTbl3Strings(){
+        return availableStrings.stream()
+                .filter(s -> s.endsWith("tbl3"))
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getAvailableStrings() {
+        return availableStrings;
+    }
+
+    public void onTbl1Drop(DragDropEvent event){
+        String stringToMove = (String) event.getData();
+        String stringEndingWithTbl1 = changeStringEnding(stringToMove, "tbl1");
+        availableStrings.remove(stringToMove);
+        availableStrings.add(stringEndingWithTbl1);
+    }
+
+    public void onTbl2Drop(DragDropEvent event){
+        String stringToMove = (String) event.getData();
+        String stringEndingWithTbl2 = changeStringEnding(stringToMove, "tbl2");
+        availableStrings.remove(stringToMove);
+        availableStrings.add(stringEndingWithTbl2);
+    }
+
+    public void onTbl3Drop(DragDropEvent event){
+        String stringToMove = (String) event.getData();
+        String stringEndingWithTbl3 = changeStringEnding(stringToMove, "tbl3");
+        availableStrings.remove(stringToMove);
+        availableStrings.add(stringEndingWithTbl3);
+    }
+
+    private String changeStringEnding(String initString, String newEnding) {
+        return initString.substring(0,2) + newEnding;
+    }
 }
